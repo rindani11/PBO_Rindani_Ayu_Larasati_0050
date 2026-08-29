57dc1ec6be4e40441ec736b060a9c5fc26d3a2db
rindani11 <rindani11@students.untidar.ac.id>
2026-08-29 19:56:29 +0700

    Add files via upload

---
+++ Rindani_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rindani_project1;
+
+/**
+ *
+ * @author DELL
+ */
+public class Rindani_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama  : Rindani Ayu Larasati");
+        System.out.println("NPM   : 2505060050 ");
+        System.out.println("Alamat: Magelang ");
+        System.out.println("No_HP : 08*********7");
+    }
+}
